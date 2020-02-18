b2f6815ff582b0b53c7bc1338a36f7f3ab63a212
Jordan Gregory <jordangregory@granular.ag>
2020-02-18 13:39:47 -0600

    make code suggestions, will add references in next commit

--- main.go
+++ main.go
@@ -1,41 +1,52 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"gopkg.in/yaml.v2"
 )
 
-// Define var for config path
-var configPath string
-
 // Config struct for webapp config
 type Config struct {
 	Server struct {
-		Host    string `yaml:"host"`
+		// Host is the local machine IP Address to bind the HTTP Server to
+		Host string `yaml:"host"`
+
+		// Port is the local machine TCP Port to bind the HTTP Server to
 		Port    string `yaml:"port"`
 		Timeout struct {
-			Write int `yaml:"write"`
-			Read  int `yaml:"read"`
-			Idle  int `yaml:"idle"`
+			// Server is the general server timeout to use for graceful shutdowns
+			Server time.Duration `yaml:"server"`
+
+			// Write is the amount of time to wait until an HTTP server write opperation is cancelled
+			Write time.Duration `yaml:"write"`
+
+			// Read is the ammout of time to wait until an HTTP server read operation is cancelled
+			Read time.Duration `yaml:"read"`
+
+			// Read is the ammout of time to wait until an IDLE HTTP session is closed
+			Idle time.Duration `yaml:"idle"`
 		} `yaml:"timeout"`
 	} `yaml:"server"`
 }
 
 // NewConfig returns a new decoded Config struct
-func NewConfig(configPath string) *Config {
+func NewConfig(configPath string) (*Config, error) {
 	// Create config structure
 	config := &Config{}
 
 	// Open config file
 	file, err := os.Open(configPath)
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 	defer file.Close()
 
@@ -44,39 +55,112 @@ func NewConfig(configPath string) *Config {
 
 	// Start YAML decoding from file
 	if err := d.Decode(&config); err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
-	return config
+	return config, nil
 }
 
-// Function will be run before main()
-func init() {
-	// Looking for config flag
-	flag.StringVar(&configPath, "config", "config.yml", "path to config file")
+// ValidateConfigPath just makes sure that the path provided is a file that can be read
+func ValidateConfigPath(path string) error {
+	s, err := os.Stat(path)
+	if err != nil {
+		return err
+	}
+	if s.IsDir() {
+		return fmt.Errorf("'%s' is a directory, not a normal file", path)
+	}
+	return nil
 }
 
-func main() {
-	// Create new config instance
-	config := NewConfig(configPath)
+// ParseFlags will create and parse the CLI flags and return the path to be used elsewhere
+func ParseFlags() (string, error) {
+	// String that contains the configured configuration path
+	var configPath string
 
-	// Create router and define routes
+	// Set up a CLI flag called "-config" to allow users to supply the configurationg file
+	flag.StringVar(&configPath, "config", "./config.yml", "path to config file")
+
+	// Acutally parse the flags
+	flag.Parse()
+
+	// Validate the path first
+	if err := ValidateConfigPath(configPath); err != nil {
+		return "", err
+	}
+
+	// Return the configuation path
+	return configPath, nil
+}
+
+// NewRouter generates the router used in the HTTP Server
+func NewRouter() *http.ServeMux {
+	// Create router and define routes and return that router
 	router := http.NewServeMux()
+
 	router.HandleFunc("/welcome", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
 	})
 
-	// Define server options
+	return router
+}
+
+// Run will run the HTTP Server
+func (config Config) Run() {
+	// Set up a channel to listen to for interrupt signals.
+	var runChan = make(chan os.Signal, 1)
+
+	// Set up a context to allow for graceful server shutdowns in the event of an OS interrupt (defers the cancel just in case).
+	ctx, cancel := context.WithTimeout(context.Background(), config.Server.Timeout.Server)
+	defer cancel()
+
+	// Define server options.
 	server := &http.Server{
 		Addr:         config.Server.Host + ":" + config.Server.Port,
-		Handler:      router,
-		ReadTimeout:  time.Duration(config.Server.Timeout.Read) * time.Second,
-		WriteTimeout: time.Duration(config.Server.Timeout.Write) * time.Second,
-		IdleTimeout:  time.Duration(config.Server.Timeout.Idle) * time.Second,
+		Handler:      NewRouter(),
+		ReadTimeout:  config.Server.Timeout.Read * time.Second,
+		WriteTimeout: config.Server.Timeout.Write * time.Second,
+		IdleTimeout:  config.Server.Timeout.Idle * time.Second,
 	}
 
-	// Start server
-	if err := server.ListenAndServe(); err != nil {
+	// Handle ctrl+c/ctrl+x interrupt.
+	signal.Notify(runChan, os.Interrupt, syscall.SIGTSTP)
+
+	// Alert the user that the server is starting and run the server on a new goroutine.
+	log.Printf("server is starting on %s\n", server.Addr)
+	go func() {
+		if err := server.ListenAndServe(); err != nil {
+			if err == http.ErrServerClosed {
+				// Normal iterrupt operation, ignore
+			} else {
+				log.Fatalf("server failed to start due to err: %+v", err)
+			}
+		}
+	}()
+
+	// Block on this channel listeninf for those previously defined syscalls.
+	// assign to variable so we can let the user know why the server is shutting down.
+	interrupt := <-runChan
+
+	// If we get one of the pre-prescribed syscalls, gracefully terminate the server while alerting the user.
+	log.Printf("server is shutting down due to %+v\n", interrupt)
+	if err := server.Shutdown(ctx); err != nil {
+		log.Fatalf("server was unable to gracefully shutdown due to err: %+v", err)
+	}
+}
+
+// main should be as small as possible and do as little as possible by convention
+func main() {
+	// Generate our config based on the config supplied by the user in the flags
+	cfgPath, err := ParseFlags()
+	if err != nil {
 		log.Fatal(err)
 	}
+	cfg, err := NewConfig(cfgPath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Run the server
+	cfg.Run()
 }
